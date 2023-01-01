63cecfc0c7f90ea8896b7ef7dfc654aaf975ae38
Laurens Hof <laurens.hof@gmail.com>
2023-01-01 20:15:48 +0100

    colors

--- src/_data/content.js
+++ src/_data/content.js
@@ -6,7 +6,7 @@ const entries = [
   {
     id: 'mastodon-releases-public-roadmap',
     categories: ['news', 'mastodon'],
-    color: 'blue',
+    color: 'grey',
     faicon: 'road',
     datetime: '2022-12-31 19:48',
     title: 'Mastodon releases public development roadmap',
@@ -26,7 +26,7 @@ const entries = [
   {
     id: 'shuttlecraft-selfhostable-microblogging-tool-implementing-activitypub',
     categories: ['software'],
-    color: 'blue',
+    color: 'grey',
     faicon: 'screwdriver-wrench',
     datetime: '2022-12-31 01:49',
     title: 'Shuttlecraft, a tool for self-hosting a microblogging tool that works with ActivityPub, released',
@@ -42,7 +42,7 @@ const entries = [
   {
     id: 'ars-technica-mastodon—and-the-pros-and-cons-of-moving-beyond-big-tech-gatekeepers',
     categories: ['analysis'],
-    color: 'blue',
+    color: 'grey',
     faicon: 'magnifying-glass',
     datetime: '2022-12-30 14:00',
     title: 'Ars Technica: Mastodon—and the pros and cons of moving beyond Big Tech gatekeepers',
@@ -64,7 +64,7 @@ const entries = [
   {
     id: 'reddit-ama-eugen-rochko',
     categories: ['mastodon'],
-    color: 'blue',
+    color: 'grey',
     faicon: 'question',
     datetime: '2022-12-21 00:00',
     title: 'Reddit AMA with Eugen Rochko',
@@ -80,7 +80,7 @@ const entries = [
   {
     id: 'twitter-suspends-mastodon-account-prevents-sharing-link-mastodon-statement',
     categories: ['news', 'mastodon', 'twitter'],
-    color: 'blue',
+    color: 'grey',
     faicon: 'hashtag',
     datetime: '2022-12-20 00:00',
     title: 'Statement by Mastodon on the suspention of links to Mastodon on Twitter',
@@ -96,7 +96,7 @@ const entries = [
   {
     id: 'automattic-tweet-future-implementation-activityPub',
     categories: ['news'],
-    color: 'blue',
+    color: 'grey',
     faicon: 'hashtag',
     datetime: '2022-11-21 08:02',
     title: 'Automattic (Tumblr, Wordpress) CEO Matt Mullenweg announces support for ActivityPub coming "ASAP"',
@@ -118,7 +118,7 @@ const entries = [
   {
     id: 'the-many-branches-of-the-fediverse',
     categories: ['explainer'],
-    color: 'blue',
+    color: 'grey',
     faicon: 'exclamation',
     date: '2022-11-02',
     title: 'The many branches of the Fediverse',
@@ -139,7 +139,7 @@ const entries = [
 ];
 
 // Page details
-const pageTitle = 'The fediverse is actually going great'; // The title of the page that shows in the browser tab
+const pageTitle = 'The Fediverse Report - A timeline of news around the fediverse'; // The title of the page that shows in the browser tab
 const pageDescription = 'A timeline documenting news around the fediverse'; // The description of the page for search engines
 const pageAuthor = 'Laurens Hof'; // Your name
 
